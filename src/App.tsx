961cfbfa87d58d75c00a38cc5b49103d41074dc0
Swan <swan.marin@epitech.eu>
2024-04-05 20:30:35 +0200

    modif

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,28 @@
  * @returns JSX.Element
  */
 
-import { useState } from 'react';
-import CreateEmployee from './pages/CreateEmployee';
-import Employees from './pages/Employees';
-import './index.css';
+import { useState } from "react";
+import CreateEmployee from "./pages/CreateEmployee";
+import Employees from "./pages/Employees";
+import "./index.css";
 
 export default () => {
-    const [employees, setEmployees] = useState(() => {
+  const [employees, setEmployees] = useState(() => {
     // État pour stocker les données des employés
-        const savedEmployees = localStorage.getItem('employees');
-        const parsedEmployees = savedEmployees ? JSON.parse(savedEmployees) : [];
-        return parsedEmployees
-    });
+    const savedEmployees = localStorage.getItem("employees");
+    const parsedEmployees = savedEmployees ? JSON.parse(savedEmployees) : [];
+    return parsedEmployees;
+  });
 
-    const [page, setPage] = useState('Home');
+  const [page, setPage] = useState("Home");
 
-    return (
-        <>
-            {page === 'Home' ? (
-                <CreateEmployee setPage={setPage} setEmployees={setEmployees} />
-            ) : (
-                <Employees setPage={setPage} employees={employees} />
-            )}
-        </>
-    );
+  return (
+    <>
+      {page === "Home" ? (
+        <CreateEmployee setPage={setPage} setEmployees={setEmployees} />
+      ) : (
+        <Employees setPage={setPage} employees={employees} />
+      )}
+    </>
+  );
 };
